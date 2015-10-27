08259c7b419c23c6fff1a694aafe32a455deb7ff
Graeme <flangemodo@hotmail.com>
2015-10-27 18:44:45 +0000

    second commit

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -3,4 +3,39 @@ class CountriesController < ApplicationController
     @countries = Country.all
   end
 
+  def update
+    country = Country.find(params[:id])
+    country.update(country_params)
+    redirect_to(countries_path)
+  end
+
+  def new
+    @country = Country.new
+  end
+
+  def create
+    Country.create( country_params )
+    redirect_to(countries_path)
+  end
+
+  def show
+    @country = Country.find(params[:id])
+  end
+
+  def destroy
+    country = Country.find(params[:id])
+    country.destroy
+    redirect_to(countries_path)
+  end
+
+  def edit
+    @country = Country.find(params[:id])
+  end
+
+  private
+
+  def country_params
+    params.require(:country).permit(:name, :flag, :size, :population, :language, :currency)
+  end
+
 end
